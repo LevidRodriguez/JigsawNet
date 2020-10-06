96ccd83d39de0647e75ed5711081a11dc153e7ed
Levid Rodriguez <levid.rodriguez@gmail.com>
2020-10-06 12:13:04 -0500

    disable_v2_behavior

--- JigsawCNN/Boost.py
+++ JigsawCNN/Boost.py
@@ -4,7 +4,9 @@ CNN boost training
 
 
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import JIgsawAbitraryNetROI
 import Parameters
 import os, glob
